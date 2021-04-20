84a98eab7d726ba7def05a900a912e7ef45dd0ae
Charlie <charlyfinos@gmail.com>
2021-04-19 23:24:44 -0300

    Add error tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import PokemonListItem from './components/PokemonListItem'
 import { getPokemons } from './services/pokemons';
 
 function App() {
-  const [pokemons, setPokemons] = React.useState([])
+  const [pokemons, setPokemons] = useState([])
 
-  React.useEffect(() => {
-    getPokemons().then(setPokemons);
+  const [errorState, setErrorState] = useState({ hasErrors: false })
+
+  useEffect(() => {
+    getPokemons().then(setPokemons).catch(handleError);
   }, []);
+
+  function handleError(err) {
+    setErrorState({ hasErrors: true, message: err.message })
+  }
   return (
     <section className="App">
+      {errorState.hasErrors && <div>{errorState.message}</div>}
       {pokemons.map((item) => (
         <PokemonListItem key={item.name} name={item.name} />
       ))}
--- src/App.test.js
+++ src/App.test.js
@@ -22,4 +22,66 @@ describe("Pokémon app", () => {
     }
   });
 
+  it("shows an error message when there's a network error", async () => {
+    //Mock
+    window.fetch.mockRejectedValueOnce(
+      new TypeError("Network connection lost")
+    );
+
+    // Render
+    render(<App />)
+
+    // Expect api to be called which an how many times
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      'https://pokeapi.co/api/v2/pokemon'
+    );
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+
+    // Check if the error appears
+    expect(
+      await screen.findByText(
+        "There was a network error. Please try again in a few seconds."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("shows an error message when there's a server error", async () => {
+    window.fetch.mockResolvedValueOnce({
+      ok: false,
+      status: 500,
+    })
+
+    render(<App />);
+
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      "https://pokeapi.co/api/v2/pokemon"
+    )
+
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+
+    expect(
+      await screen.findByText("There was a server error.")
+    ).toBeInTheDocument();
+
+  });
+
+  it("shows an error message when there's a service not found", async () => {
+    window.fetch.mockResolvedValueOnce({
+      ok: false,
+      status: 404,
+    });
+
+    render(<App />)
+
+    expect(window.fetch).toHaveBeenCalledWith(
+      "https://pokeapi.co/api/v2/pokemon"
+    );
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+
+    expect(
+      await screen.findByText("The requested resource was a not found.")
+    ).toBeInTheDocument();
+  })
+
+
 });
